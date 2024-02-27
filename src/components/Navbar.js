46bbbce510d951ba07e3243e507b1912259edb4e
Dhitosh Roy <142252404+DhitoshRoy@users.noreply.github.com>
2024-02-27 22:15:39 +0530

    first comit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const Navbar = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            {/* <span class="navbar-brand mb-0 h1">Navbar</span> */}
+            <div className='container-fluid'>
+                <Link to='/' className='navbar-brand ml-5'> Contact List Application </Link>
+            </div>
+        </nav >
+    )
+
+}
+export default Navbar
